b786fc60a304305e31ee0c41c3700d2300fad9ec
ChaeHo95 <chlcogh11@gmail.com>
2023-05-06 19:09:02 +0900

    fix : 모바일 접속 페이지 video 가동 방식 수정

--- src/layout/mobile/Main.tsx
+++ src/layout/mobile/Main.tsx
@@ -1,10 +1,20 @@
 import "../../res/style/layout/main.scss"
-const Main = () => {
 
-    return(
+const Main = () => {
+    const videoPlay = setInterval(() => {
+        const video = document.querySelector(".mobile__background") as HTMLVideoElement;
+        if (video instanceof HTMLVideoElement) {
+            if(video.paused){
+                video.play();
+            }else {
+                clearInterval(videoPlay);
+            }
+        }
+    }, 1000)
+    return (
         <div id={"main"}>
-            <video className={"mobile__background"} muted autoPlay loop>
-                <source src={require("../../res/mp4/mobile_background.mp4")} type="video/mp4" />
+            <video className={"mobile__background"} muted={true} autoPlay={true} loop={true}>
+                <source src={require("../../res/mp4/mobile_background.mp4")} type="video/mp4"/>
             </video>
             <div className={"mobile__description"}>
                 PC 버전으로만 접속이 가능합니다.<br/>
