372dc339dbfccd330dbaba89906f1d7080f3ff1e
ChaeHo95 <chlcogh11@gmail.com>
2023-05-06 18:49:16 +0900

    feat : 모바일 접속 페이지 UI 수정

--- src/layout/mobile/Main.tsx
+++ src/layout/mobile/Main.tsx
@@ -3,6 +3,9 @@ const Main = () => {
 
     return(
         <div id={"main"}>
+            <video className={"mobile__background"} muted autoPlay loop>
+                <source src={require("../../res/mp4/mobile_background.mp4")} type="video/mp4" />
+            </video>
             <div className={"mobile__description"}>
                 PC 버전으로만 접속이 가능합니다.<br/>
                 모바일은 지원 하지 않습니다.
