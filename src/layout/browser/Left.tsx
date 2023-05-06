4824caaf1a9cfaedee14ec88d96949f70c67c3f6
ChaeHo95 <chlcogh11@gmail.com>
2023-05-07 00:23:32 +0900

    feat : 반응형 적용 및 프로필 이미지 삽입

--- src/layout/browser/Left.tsx
+++ src/layout/browser/Left.tsx
@@ -3,6 +3,9 @@ const Left = () => {
 
     return(
         <div id={"left"}>
+            <div className={"left__img-box"}>
+                <img src={require("../../res/img/my_profile.JPG")} />
+            </div>
         </div>
     )
 }
