d99d3aa141a9336269a5047b4966dceb33224e4c
ChaeHo95 <chlcogh11@gmail.com>
2023-05-06 14:32:14 +0900

    fix : restart

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
 import React from 'react';
 import './App.scss';
-import {BrowserView, MobileView} from 'react-device-detect';
-import {BrowserRouter} from "react-router-dom";
-import Browser from "./browser/Browser";
-import Mobile from "./mobile/Mobile";
 
 const App = () => {
 
     return (
         <div className="App">
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
-                {/** 브라우저 */}
-                <BrowserView style={styles.container}>
-                    <Browser/>
-                </BrowserView>
 
-                {/** 모바일 */}
-                <MobileView style={styles.container}>
-                    <Mobile/>
-                </MobileView>
-            </BrowserRouter>
         </div>
     );
-}
-
-const styles = {
-    container: {width: "100vw", height: "100vh"}
 }
 
 export default App;
--- src/component/index.ts
+++
@@ -1,2 +0,0 @@
-/** auth 영역*/
-export {default as Auth} from "./auth/Auth";
